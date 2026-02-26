41b69a24396ca10ade72fac2b4a12c539232c008
codebydoh <main@dohtechsolutions.com>
2026-02-26 09:17:12 -0600

    added deployment setups

--- src/core/database/database.module.ts
+++ src/core/database/database.module.ts
@@ -34,7 +34,7 @@ import { ResourceInteraction } from '../../modules/resources/entities/resource-i
                     Resource, ResourceInteraction,
                 ],
                 autoLoadModels: true,
-                synchronize: false,
+                synchronize: config.get<boolean>('DB_SYNC', false),
                 logging: config.get<string>('NODE_ENV') === 'development' ? console.log : false,
             }),
         }),
